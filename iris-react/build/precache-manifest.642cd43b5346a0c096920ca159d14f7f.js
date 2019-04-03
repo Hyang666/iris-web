ffb11b2e36dc83a6be21455a47c69d215f9fd80f
Evan Li <evan.li@coupang.com>
2019-04-03 17:55:47 +0800

    release the build file

---
+++ iris-react/build/precache-manifest.642cd43b5346a0c096920ca159d14f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "73207d7b6dfaddd54d95",
+    "url": "./static/js/main.4d862e02.chunk.js"
+  },
+  {
+    "revision": "c773441dad233d2ab0d5",
+    "url": "./static/js/2.fac573de.chunk.js"
+  },
+  {
+    "revision": "73207d7b6dfaddd54d95",
+    "url": "./static/css/main.d65b4aee.chunk.css"
+  },
+  {
+    "revision": "c773441dad233d2ab0d5",
+    "url": "./static/css/2.d9b81e9c.chunk.css"
+  },
+  {
+    "revision": "0455b88efa3791c6ccfe624690134a37",
+    "url": "./index.html"
+  }
+];
